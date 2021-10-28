a636602019304eef32e5fa4a64623c8fbc4dc13b
bruno martins vilas boas <bruno.martinsvilasboas@ynov.com>
2021-10-28 10:45:58 +0200

    js notif

--- static/js/header.js
+++ static/js/header.js
@@ -1,3 +1,31 @@
+
+var params = new Object();
+    params.action = "getStats";
+    params.what = "Post";
+    fetch("/fetching", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify(params),
+    })
+      .then((x) => x.json()).then((x) => {console.log(x.DataChart[0].Count);
+        if(x.DataChart[0].Count = 98){
+            var elem =  document.getElementById("frind") ;
+    
+            if(getComputedStyle(elem).display != "none"){
+                
+                elem.style.display = "none";
+            }else{
+                elem.style.display = "block"; 
+            }
+        }
+    
+    });
+ 
+      
+  
 function friend(){
     var elem =  document.getElementById("frind") ;
     
